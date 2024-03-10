b80bd69518df0f671c7566e6b4ed3bdd7c4e1c42
GondorGitaros <csaszardaniel81@gmail.com>
2024-03-10 19:20:54 +0100

    update

--- sudukugen.py
+++ sudukugen.py
@@ -16,16 +16,33 @@ def print_board(board):
         for col in range(len(board[row])):
             col+=1
             if col % 3 == 0 and col != 9:
-                print(board[row][col-1], end='|')
+                if board[row][col-1] == '0':
+                    print(' ', end=' || ')
+                else:
+                    print(board[row][col-1], end=' || ')
             else:
-                print(board[row][col-1], end=' ')
+                if board[row][col-1] == '0':
+                    print(' ', end=' | ')
+                else:
+                    print(board[row][col-1], end=' | ')
         row+=1
         if row % 3 == 0 and row != 9:
-            print('\n' + '------' * 3)
+            print('\n' + '______' * 6)
         else:
-            print()
+            print('\n' + '------' * 6)
 
 def randnum():
     return random.randint(1,9)
 
-print(randnum())
+seen = []
+
+for i in range(3):
+    for j in range(3):
+        possiblenum = str(randnum())
+        if str(possiblenum) in seen:
+            pass
+        else:
+            board[i][j] = possiblenum
+            seen += board[i][j]
+
+print_board(board)
